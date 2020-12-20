8aa64b26760893515c0d0c6ac532308f35f8a798
Abdghali <32044169+Abdghali@users.noreply.github.com>
2020-12-20 22:03:13 +0200

    Update images_moke.dart

--- lib/images_moke.dart
+++ lib/images_moke.dart
@@ -8,14 +8,7 @@
     'assets/images/h4.jpg',
     'assets/images/h5.jpg',
     'assets/images/h6.jpg',
-    'assets/images/h7.jpg',
-    'assets/images/h8.jpg',
-    'assets/images/h9.jpg',
-    'assets/images/h10.jpg',
-    'assets/images/h11.jpg',
-    'assets/images/h12.jpg',
-    'assets/images/h13.jpg',
-    'assets/images/h14.jpg',
+    
 
   ];
   
